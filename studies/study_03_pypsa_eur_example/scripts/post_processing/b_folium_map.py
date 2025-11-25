866d3e404f2f31bb37cd1a26a763b93720985699
helgestefan <on@hstes.de>
2025-11-25 11:07:45 +0100

    refactors folium legend system location

--- studies/study_03_pypsa_eur_example/scripts/post_processing/b_folium_map.py
+++ studies/study_03_pypsa_eur_example/scripts/post_processing/b_folium_map.py
@@ -1,17 +1,4 @@
-import folium
-
 from mesqual import StudyManager, _kpis_deprecated
-from mesqual.units import Units
-from mesqual.visualizations.value_mapping_system import SegmentedContinuousColorscale
-from mesqual.visualizations.folium_legend_system import ContinuousColorscaleLegend
-from mesqual.visualizations.folium_viz_system import (
-    PropertyMapper,
-    KPIDataItem,
-    KPICollectionMapVisualizer,
-    AreaGenerator, AreaFeatureResolver,
-    TextOverlayGenerator, TextOverlayFeatureResolver,
-)
-from mesqual.utils.folium_utils.background_color import set_background_color_of_map
 from mesqual._kpis_deprecated import KPI
 
 study: StudyManager
