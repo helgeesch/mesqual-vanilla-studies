075f1111657cdc7c4074bd1d143b0fe46cc95ba7
helgestefan <on@hstes.de>
2026-02-16 11:02:22 +0100

    updates mkdocs ignore

--- studies/study_02_pypsa_eur_example/scripts/b_post_processing/b_simple_fetch.py
+++ studies/study_02_pypsa_eur_example/scripts/b_post_processing/b_simple_fetch.py
@@ -3,24 +3,35 @@ if __name__ == '__main__':
     study: StudyManager
     (study,)
 
-    flag = 'countries_t.trade_balance_per_partner'
+    model_flag = 'countries'
+    result_flag = 'countries_t.trade_balance_per_partner'
 
     print(f'\n\n', '='*50, '\n')
 
-    print(f'All scenarios → MultiIndex DataFrame\n')
-    print(study.scen.fetch(flag).round(2))
+    print(f'Results: All scenarios → MultiIndex DataFrame\n')
+    print(study.scen.fetch(result_flag).round(2))
 
     print(f'\n\n', '='*50, '\n')
 
-    print(f'Individual scenario\n')
-    print(study.scen.get_dataset('base').fetch(flag).round(2))
+    print(f'Results: Individual scenario\n')
+    print(study.scen.get_dataset('base').fetch(result_flag).round(2))
 
     print(f'\n\n', '='*50, '\n')
 
-    print(f'All comparisons (deltas) → same interface\n')
-    print(study.comp.fetch(flag).round(2))
+    print(f'Model: Individual scenario\n')
+    print(study.scen.get_dataset('base').fetch(model_flag))
 
     print(f'\n\n', '='*50, '\n')
 
-    print(f'Combined fetch: scenarios + comparisons (additional Index-level)\n')
-    print(study.scen_comp.fetch(flag).round(2))
+    print(f'Results: All comparisons (deltas) → same interface\n')
+    print(study.comp.fetch(result_flag).round(2))
+
+    print(f'\n\n', '='*50, '\n')
+
+    print(f'Results: Combined fetch: scenarios + comparisons (additional Index-level)\n')
+    print(study.scen_comp.fetch(result_flag).round(2))
+
+    print(f'\n\n', '='*50, '\n')
+
+    print(f'Results: Combined fetch: scenarios + comparisons (additional Index-level)\n')
+    print(study.scen_comp.fetch(result_flag).round(2))
